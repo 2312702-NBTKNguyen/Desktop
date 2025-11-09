c94b7d8d2608b244fa8112b8f949ac7b38518763
2312702-NBTKNguyen <2312702@dlu.edu.vn>
2025-11-09 22:46:25 +0700

    Add ChuDe3 & ChuDe7

---
+++ 2312702_NBTKNguyen_ChuDe7/Lab07_Entity Framework/Lab07_Entity Framework/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Lab07_Entity_Framework
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReloadCategory = new System.Windows.Forms.Button();
+            this.btnAddCategory = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnAddFood = new System.Windows.Forms.Button();
+            this.btnReloadFood = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tvwCategory = new System.Windows.Forms.TreeView();
+            this.lvwFood = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh mục";
+            // 
+            // btnReloadCategory
+            // 
+            this.btnReloadCategory.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReloadCategory.Location = new System.Drawing.Point(181, 9);
+            this.btnReloadCategory.Name = "btnReloadCategory";
+            this.btnReloadCategory.Size = new System.Drawing.Size(41, 28);
+            this.btnReloadCategory.TabIndex = 1;
+            this.btnReloadCategory.Text = "R";
+            this.toolTip1.SetToolTip(this.btnReloadCategory, "Tải lại danh mục");
+            this.btnReloadCategory.UseVisualStyleBackColor = true;
+            this.btnReloadCategory.Click += new System.EventHandler(this.btnReloadCategory_Click);
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCategory.Location = new System.Drawing.Point(228, 9);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(41, 28);
+            this.btnAddCategory.TabIndex = 2;
+            this.btnAddCategory.Text = "+";
+            this.toolTip1.SetToolTip(this.btnAddCategory, "Thêm danh mục mới");
+            this.btnAddCategory.UseVisualStyleBackColor = true;
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddFood.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddFood.Location = new System.Drawing.Point(910, 9);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(41, 28);
+            this.btnAddFood.TabIndex = 4;
+            this.btnAddFood.Text = "+";
+            this.toolTip1.SetToolTip(this.btnAddFood, "Thêm món ăn mới");
+            this.btnAddFood.UseVisualStyleBackColor = true;
+            // 
+            // btnReloadFood
+            // 
+            this.btnReloadFood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReloadFood.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReloadFood.Location = new System.Drawing.Point(816, 9);
+            this.btnReloadFood.Name = "btnReloadFood";
+            this.btnReloadFood.Size = new System.Drawing.Size(41, 28);
+            this.btnReloadFood.TabIndex = 3;
+            this.btnReloadFood.Text = "R";
+            this.toolTip1.SetToolTip(this.btnReloadFood, "Tải lại danh sách món ăn");
+            this.btnReloadFood.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(863, 9);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(41, 28);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "-";
+            this.toolTip1.SetToolTip(this.btnDelete, "Xóa món ăn được chọn");
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(296, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Thực đơn";
+            // 
+            // tvwCategory
+            // 
+            this.tvwCategory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tvwCategory.Location = new System.Drawing.Point(16, 43);
+            this.tvwCategory.Name = "tvwCategory";
+            this.tvwCategory.Size = new System.Drawing.Size(253, 348);
+            this.tvwCategory.TabIndex = 7;
+            this.tvwCategory.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvwCategory_AfterSelect);
+            // 
+            // lvwFood
+            // 
+            this.lvwFood.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwFood.FullRowSelect = true;
+            this.lvwFood.GridLines = true;
+            this.lvwFood.HideSelection = false;
+            this.lvwFood.Location = new System.Drawing.Point(300, 43);
+            this.lvwFood.MultiSelect = false;
+            this.lvwFood.Name = "lvwFood";
+            this.lvwFood.Size = new System.Drawing.Size(651, 348);
+            this.lvwFood.TabIndex = 8;
+            this.lvwFood.UseCompatibleStateImageBehavior = false;
+            this.lvwFood.View = System.Windows.Forms.View.Details;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 403);
+            this.Controls.Add(this.lvwFood);
+            this.Controls.Add(this.tvwCategory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddFood);
+            this.Controls.Add(this.btnReloadFood);
+            this.Controls.Add(this.btnAddCategory);
+            this.Controls.Add(this.btnReloadCategory);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý nhà hàng";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReloadCategory;
+        private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnAddFood;
+        private System.Windows.Forms.Button btnReloadFood;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView tvwCategory;
+        private System.Windows.Forms.ListView lvwFood;
+    }
+}
+
---
+++ 2312702_NBTKNguyen_ChuDe7/Lab07_Entity Framework/Lab07_Entity Framework/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using Lab07_Entity_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07_Entity_Framework
+{
+    public partial class MainForm : Form
+    {
+        private List<Category> GetCategories()
+        {
+            var dbContext = new RestaurantContext();
+            return dbContext.Categories.OrderBy(x => x.Name).ToList();
+        }
+
+        private void ShowCategories()
+        {
+            tvwCategory.Nodes.Clear();
+
+            var cateMap = new Dictionary<CategoryType, string>
+            {
+                [CategoryType.Food] = "Đồ ăn",
+                [CategoryType.Drink] = "Thức uống"
+            };
+
+            var rootNode = tvwCategory.Nodes.Add("Tất cả");
+            var categories = GetCategories();
+
+            foreach (var cateType in cateMap)
+            {
+                var childNode = rootNode.Nodes.Add(cateType.Key.ToString(), cateType.Value);
+                childNode.Tag = cateType.Key;
+
+                foreach (var category in categories)
+                {
+                    if (category.Type != cateType.Key) continue;
+
+                    var grandChildNode = childNode.Nodes.Add(category.Id.ToString(), category.Name);
+                    grandChildNode.Tag = category;
+                }
+            }
+            tvwCategory.ExpandAll();
+            tvwCategory.SelectedNode = rootNode;
+
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ShowCategories();
+        }
+
+        private void btnReloadCategory_Click(object sender, EventArgs e)
+        {
+            ShowCategories();
+        }
+        private List<FoodModel> GetFoodByCategory(int categoryId)
+        {
+            var dbContext = new RestaurantContext();
+            var foods = dbContext.Foods.AsQueryable();
+
+            if (categoryId != null && categoryId > 0)
+            {
+                foods = foods.Where(x => x.FoodCategoryId == categoryId);
+            }
+
+            return foods
+                .OrderBy(x => x.Name)
+                .Select(x => new FoodModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Unit = x.Unit,
+                    Price = x.Price,
+                    Notes = x.Notes,
+                    CategoryName = x.Category.Name
+                })
+                .ToList();
+        }
+
+        private List<FoodModel> GetFoodByCategoryType(CategoryType cateType)
+        {
+            var dbContext = new RestaurantContext();
+
+            return dbContext.Foods
+                .Where(x => x.Category.Type == cateType)
+                .OrderBy(x => x.Name)
+                .Select(x => new FoodModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Unit = x.Unit,
+                    Price = x.Price,
+                    Notes = x.Notes,
+                    CategoryName = x.Category.Name
+                })
+                .ToList();
+        }
+        private void ShowFoodsForNode(TreeNode node)
+        {
+            lvwFood.Items.Clear();
+            if (node == null) return;
+
+            List<FoodModel> foods = null;
+
+            if (node.Level == 1)
+            {
+                var categoryType = (CategoryType)node.Tag;
+                foods = GetFoodByCategoryType(categoryType);
+            }
+            else
+            {
+                var category = node.Tag as Category;
+                foods = GetFoodByCategory(category?.Id ?? 0);
+            }
+
+            ShowFoodsOnListView(foods);
+        }
+
+        private void ShowFoodsOnListView(List<FoodModel> foods)
+        {
+            foreach (var foodItem in foods)
+            {
+                var item = lvwFood.Items.Add(foodItem.Id.ToString());
+                item.SubItems.Add(foodItem.Name);
+                item.SubItems.Add(foodItem.Unit);
+                item.SubItems.Add(foodItem.Price.ToString("#,##0"));
+                item.SubItems.Add(foodItem.CategoryName);
+                item.SubItems.Add(foodItem.Notes);
+            }
+        }
+
+        private void tvwCategory_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ShowFoodsForNode(e.Node);
+        }
+    }
+}
